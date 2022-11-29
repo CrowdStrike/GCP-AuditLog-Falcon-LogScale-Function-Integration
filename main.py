7b05b01795806e4555c1c6174e4e5a2a7fb088de
Joshua Hiller <74007258+jshcodes@users.noreply.github.com>
2022-11-28 20:06:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ import urllib3
 http = urllib3.PoolManager()
 dest_url = "https://cloud.community.humio.com/api/v1/ingest/hec/raw"
 dest_token1 = <xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx>
-header1 = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + dest_token1}
+header1 = {"Content-Type": "application/json", "Authorization": f"Bearer {dest_token1}"}
 
 def send_data(event, context):
     """Send the event to LogScale."""
 
-    print("""This Function was triggered by messageId {} published at {} to {}
-    """.format(context.event_id, context.timestamp, context.resource["name"]))
-
+    print(f"This function was triggered by messageId {context.event_id} "
+          f"published at {context.timestamp} to {context.resource['name']}"
+          )
+    
     if 'data' in event:
 
         newevent = base64.b64decode(event['data']).decode('utf-8')
